706ac74ca0b275fe2ab635febef39905da16bb20
Misstes <you@example.com>
2024-06-03 16:44:15 -0500

    Update SalesController to handle new SaleService methods:
    - Modify SalesController to include new endpoints for creating, finding all, finding one, updating, and deleting sales.
    - Ensure that the SalesController routes are properly mapped to the corresponding SaleService methods.
    - No changes were made to the SalesService or other related files (e.g., Sale entity, DTOs) in this commit.
    
    This commit ensures that the SalesController is aligned with the new SaleService methods and provides a clear separation of concerns between the controller and service layers.

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { BookService } from './books.service';
+import { CreateBookDto, UpdateBookDto } from 'src/shared/dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+
+  constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.bookService.create(createBookDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.bookService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.bookService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updateBookDto: UpdateBookDto) {
+    return this.bookService.update(id, updateBookDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.bookService.remove(id);
+  }
+}
